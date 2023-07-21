f762db76572ef059e45fb763a0d1352a29041cdd
daniel-scho <bs.daniel.scho@gmail.com>
2023-07-21 14:46:40 +0200

    add new mappings in StocksController

--- stock-notifier/src/main/java/com/daniel/stocknotifier/controller/StocksController.java
+++ stock-notifier/src/main/java/com/daniel/stocknotifier/controller/StocksController.java
@@ -1,18 +1,33 @@
 package com.daniel.stocknotifier.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/stocks")
 public class StocksController {
 
-    @GetMapping("")
+    @GetMapping("/hallo1")
     public String helloWorld() {
         return "hallo wie gehts";
     }
 
-    @GetMapping("/api/hallo")
+    @GetMapping("hallo2")
     public String helloWorld1() {
         return "hallo das ist api/hallo";
+    }
+
+    @PostMapping("add")
+    public String addStock() {
+        return "stocks added";
+    }
+
+    @PutMapping("update")
+    public String updateStock() {
+        return "stocks updated";
+    }
+
+    @DeleteMapping("delete")
+    public String deleteStock() {
+        return "stocks deleted";
     }
 }
