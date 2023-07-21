4e034007dcb547f0c6702b2093b4cfe350906b0c
daniel-scho <bs.daniel.scho@gmail.com>
2023-07-21 14:15:25 +0200

    init commit: initialize spring boot project

---
+++ stock-notifier/src/main/java/com/daniel/stocknotifier/StockNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.stocknotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockNotifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockNotifierApplication.class, args);
+	}
+
+}
---
+++ stock-notifier/src/test/java/com/daniel/stocknotifier/StockNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.stocknotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
